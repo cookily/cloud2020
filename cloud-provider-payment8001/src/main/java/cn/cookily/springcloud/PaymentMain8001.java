d564345afc4b60b56e75c93a342c383b9c6b5181
chenyl <824581565@qq.com>
2020-10-13 18:21:47 +0800

    SpringCloud2020 学习笔记（四）cloud-provider-payment8001支付模块（上）

---
+++ cloud-provider-payment8001/src/main/java/cn/cookily/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package cn.cookily.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cookily
+ * @date 2020-10-12
+ */
+@SpringBootApplication
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
