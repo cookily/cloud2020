bc9edcf85e04a214bd2d34c803dc3acd79e62ba7
chenyl <824581565@qq.com>
2020-10-14 21:18:35 +0800

    SpringCloud2020 学习笔记（八）cloud-provider-payment8001支付模块入驻eurekaServer注册中心

--- cloud-provider-payment8001/src/main/java/cn/cookily/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/cookily/springcloud/PaymentMain8001.java
@@ -2,14 +2,15 @@ package cn.cookily.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cookily
  * @date 2020-10-12
  */
 @SpringBootApplication
-public class PaymentMain8001
-{
+@EnableEurekaClient
+public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
     }
