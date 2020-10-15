37f85182abda84a995ae40118f8387dd648f1a76
chenyl <824581565@qq.com>
2020-10-15 20:10:32 +0800

    SpringCloud2020 学习笔记（九）cloud-consumer-order80 消费者订单模块入驻eurekaServer注册中心

--- cloud-consumer-order80/src/main/java/cn/cookily/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/cookily/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package cn.cookily.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cookily
  * @date 2020-10-14
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
